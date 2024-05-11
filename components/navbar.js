39a837ea10d79dc024280bfb31e0caf32a78e50a
SS-Reddys <sujithreddysudini@gmail.com>
2024-05-11 13:27:55 -0400

    Updated .gitignore, package-lock.json, Login.jsx, and vite.config.js

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,7 @@ export const navigationBar = (token, name) => {
     return (
         `<nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid" id="navItems">
-                <a class="navbar-brand" href="../index.html">Food Store</a>
+                <a class="navbar-brand" href="../index.html">Food Delight</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
